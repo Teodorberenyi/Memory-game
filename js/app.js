8b2be3e75726470c7946297971b784f5822b9695
Teodr Berenyi <teodorberenyi@gmail.com>
2018-06-18 14:00:42 +0100

    Added all variables in JS

--- js/app.js
+++ js/app.js
@@ -36,13 +36,6 @@
     'cardClass' : 'fa fa-paper-plane-o'}
  ];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -56,16 +49,21 @@ function shuffle(array) {
     }
 
     return array;
-}
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+ }
+//Declaring variables
+const deck = document.getElementById('deck');
+let counter = document.querySelector(".moves");
+let matchCards = document.getElementsByClassName('match');
+const modal = document.getElementById('myModal');
+let stars = document.querySelectorAll(".stars li");
+let span = document.getElementsByClassName("close")[0];
+let timer = document.querySelector(".timer");
+let popup = document.querySelector(".winText");
+let count = [];
+//Variables for time moves and rating
+let moves = 0;
+let hours = 0;
+let minutes = 0;
+let seconds =0;
+let rating = 3;
+let interval;
